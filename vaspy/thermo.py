bde5f46ae488c4e23bb1a1a9dacc326abc13cf47
PytLab <shaozhengjiang@gmail.com>
2015-08-06 17:58:32 +0800

    Initial commit

---
+++ vaspy/thermo.py
@@ -0,0 +1,68 @@
+# -*- coding:utf-8 -*-
+import re
+
+import numpy as np
+
+from vaspy import VasPy
+
+
+class OsziCar(VasPy):
+    def __init__(self, filename='OSZICAR'):
+        VasPy.__init__(self, filename)
+
+        #set regex patterns
+        float_regex = r'[\+|-]?\d*\.\d+(?:E[\+|-]?\d+)?'
+        step = r'\s*(\d+)\s*'
+        F = r'F\=\s*(' + float_regex + r')\s*'
+        E0 = r'E0\=\s*(' + float_regex + r')\s*'
+        dE = r'd\s*E\s*\=(' + float_regex + r')\s*'
+        mag = r'mag\=\s*(' + float_regex + r')\s*'
+        self.regex = re.compile(step + F + E0 + dE + mag)
+
+        self.load()
+
+    def match(self, line):
+        m = self.regex.search(line)
+        if m:
+            step, F, E0, dE, mag = m.groups()
+            #type convertion
+            step = int(step)
+            F, E0, dE, mag = [float(i) for i in m.groups()[1:]]
+            return step, F, E0, dE, mag
+        else:
+            return None
+
+    def load(self):
+        with open(self.filename, 'r') as f:
+            steps, Fs, E0s, dEs, mags = [], [], [], [], []
+            for line in f:
+                data = self.match(line)
+                if data:  # if matched
+                    step, F, E0, dE, mag = data
+                    steps.append(step)
+                    Fs.append(F)
+                    E0s.append(E0)
+                    dEs.append(dE)
+                    mags.append(mag)
+        #set class attrs
+        self.step = np.array(steps)
+        self.F = np.array(Fs)
+        self.E0 = np.array(E0s)
+        self.dE = np.array(dEs)
+        self.mags = np.array(mags)
+
+        return
+
+    def esort(self, n, reverse=False):
+        '''
+        进行能量排序, 获取排序后的前n个值.
+        '''
+        zipped = zip(self.E0, self.step)  # (E0, step)
+        dtype = [('E0', float), ('step', int)]
+        zipped = np.array(zipped, dtype=dtype)
+        srted = np.sort(zipped, order='E0')
+
+        if reverse:
+            return srted[-n:]
+        else:
+            return srted[:n]
