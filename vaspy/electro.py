a9e6540c11f17e9ac9b6b62e8af8098f5089acce
PytLab <shaozhengjiang@gmail.com>
2015-09-12 15:23:41 +0800

    add get_dband_center method

--- vaspy/electro.py
+++ vaspy/electro.py
@@ -11,6 +11,7 @@ Updated by PytLab <shaozhengjiang@gmail.com>, September 2015
 import copy
 
 import numpy as np
+from scipy.integrate import simps
 import matplotlib.pyplot as plt
 
 from plotter import DataPlotter
@@ -87,7 +88,7 @@ class DosX(DataPlotter):
         ax.plot(x, y, linewidth=5, color='#104E8B')
         #plot fermi energy auxiliary line
         ax.plot(xfermi, yfermi, linestyle='dashed', color='#000000')
-        ax.set_xlabel(r'$\bf{E-E_F(eV)}$', fontdict={'fontsize': 20})
+        ax.set_xlabel(r'$\bf{E - E_F(eV)}$', fontdict={'fontsize': 20})
         ax.set_ylabel(r'$\bf{pDOS(arb. unit)}$', fontdict={'fontsize': 20})
         fig.show()
 
@@ -105,3 +106,24 @@ class DosX(DataPlotter):
             f.write(content)
 
         return
+
+    def get_dband_center(self):
+        "Get d-band center of the DosX object."
+        #合并d轨道DOS
+        if self.data.shape[1] == 10:
+            yd = np.sum(self.data[:, 5:10], axis=1)
+        #获取feimi能级索引
+        for idx, E in enumerate(self.data[:, 0]):
+            if E >= 0:
+                nfermi = idx
+                break
+        E = self.data[: nfermi+1, 0]  # negative inf to Fermi
+        dos = yd[: nfermi+1]  # y values from negative inf to Fermi
+        #use Simpson integration to get d-electron number
+        nelectro = simps(dos, E)
+        #get total energy of dband
+        tot_E = simps(E*dos, E)
+        dband_center = tot_E/nelectro
+        self.dband_center = dband_center
+
+        return dband_center
