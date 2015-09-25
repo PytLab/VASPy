8158c4cae39802fa9266aa451f21ca566ef9dd2a
PytLab <shaozhengjiang@gmail.com>
2015-09-25 10:59:24 +0800

    add file existance check

--- scripts/create_xsd.py
+++ scripts/create_xsd.py
@@ -2,10 +2,14 @@
     Script to convert CONTCAR to .xsd file
 '''
 import commands
+import sys
 from vaspy.matstudio import XsdFile
 from vaspy.atomco import PosCar
 
 status, output = commands.getstatusoutput('ls *.xsd | head -1')
+if not output.endswith('.xsd'):
+    print "No .xsd file in current directory."
+    sys.exit(1)
 xsd = XsdFile(filename=output)
 poscar = PosCar(filename='CONTCAR')
 xsd.data = poscar.data
