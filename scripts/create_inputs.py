1321609ee22419e9ac1a6d68d23737afff7dc5a3
PytLab <shaozhengjiang@gmail.com>
2016-05-04 18:08:48 +0800

    Add new optional arguments to create_input.py.
    1. --nnode, node number used by the job.
    2. --ncpu, number of cpu used by each node.
    3. --queue, queue type.

--- scripts/create_inputs.py
+++ scripts/create_inputs.py
@@ -5,6 +5,7 @@
 import argparse
 import commands
 import os
+import re
 import sys
 import logging
 
@@ -18,18 +19,22 @@ if "__main__" == __name__:
     # Set argument parser.
     parser = argparse.ArgumentParser()
 
-    # Add kpoint optional argument.
+    # Add optional argument.
     parser.add_argument("-k", "--kpoints", help="set k-points")
+    parser.add_argument("--nnode", help="node number used for the job")
+    parser.add_argument("--ncpu", help="cpu number on each node")
+    parser.add_argument("-q", "--queue", help="pbs queue type")
+
     args = parser.parse_args()
 
-    # create POSCAR
+    # Create POSCAR
     status, output = commands.getstatusoutput('ls *.xsd | head -1')
     xsd = XsdFile(filename=output)
     poscar_content = xsd.get_poscar_content(bases_const=1.0)
     with open('POSCAR', 'w') as f:
         f.write(poscar_content)
 
-    # create POTCAR
+    # Create POTCAR
     potdir = r'/data/pot/vasp/potpaw_PBE2010/'
     # delete old POTCAR
     if os.path.exists('./POTCAR'):
@@ -44,7 +49,7 @@ if "__main__" == __name__:
             sys.exit(1)
         commands.getstatusoutput('cat ' + potcar + ' >> ./POTCAR')
 
-    # creat KPOINTS
+    # Creat KPOINTS
     if not args.kpoints:
         kpoints = []
         for base in xsd.bases:
@@ -59,36 +64,58 @@ if "__main__" == __name__:
     with open('KPOINTS', 'w') as f:
         f.write(kpt_content)
 
-    # copy INCAR vasp.script
+    # Copy INCAR vasp.script
     commands.getstatusoutput('cp $HOME/example/INCAR $HOME/example/vasp.script ./')
-    # change jobname
+   
+    # Get content line list.
     jobname = output.split('.')[0]
     with open('vasp.script', 'r') as f:
         content_list = f.readlines()
 
+    # Change job name.
     content_list[1] = '#PBS -N ' + jobname + '\n'
+    logging.info("job name -> {}".format(jobname))
+
+    # Change node number and cpu number. 
+    if args.nnode or args.ncpu:
+        regex = re.compile(r'nodes=(\d):ppn=(\d)')
+        match = regex.search(content_list[5])
+        if not match:
+            msg = "Regular expressioon match error, please check your pbs script."
+            raise ValueError(msg)
+        nnode, ncpu = match.groups()
+        nnode = args.nnode if args.nnode else nnode
+        ncpu = args.ncpu if args.ncpu else ncpu
+        content_list[5] = "#PBS -l nodes={}:ppn={}\n".format(nnode, ncpu)
+        logging.info("nodes -> {}, ppn -> {}".format(nnode, ncpu))
+
+    # Change node type.
+    if args.queue:
+        content_list[6] = "#PBS -q {}\n".format(args.queue)
+        logging.info("queue type -> {}".format(args.queue))
+
     with open('vasp.script', 'w') as f:
         f.writelines(content_list)
 
-    # create fort.188
+    # Create fort.188
     atom_idxs = []
     atom_names = []
     for idx, atom_name in enumerate(xsd.atom_names):
         if atom_name.endswith('_c'):
             atom_idxs.append(idx)
             atom_names.append(atom_name)
-    # if constrained get distance and create fort.188
+    # If constrained get distance and create fort.188
     if atom_idxs:
         if len(atom_idxs) > 2:
             raise ValueError("More than two atoms end with '_c'")
         pt1, pt2 = [xsd.data[idx, :] for idx in atom_idxs]
-        # use Ax=b convert to cartisan coordinate
+        # Use Ax=b convert to cartisan coordinate
         diff = pt1 - pt2
         A = np.matrix(xsd.bases.T)
         x = np.matrix(diff).T
         b = A*x
         distance = np.linalg.norm(b)
-        # create fort.188
+        # Create fort.188
         content = '1\n3\n6\n4\n0.04\n%-5d%-5d%f\n0\n' % \
             (atom_idxs[0]+1, atom_idxs[1]+1, distance)
         with open('fort.188', 'w') as f:
@@ -100,7 +127,7 @@ if "__main__" == __name__:
         logging.info("distance: {:f}".format(distance))
         logging.info('-'*20)
 
-        # set IBRION = 1
+        # Set IBRION = 1
         incar = InCar()
         incar.set('IBRION', 1)
         incar.tofile()
