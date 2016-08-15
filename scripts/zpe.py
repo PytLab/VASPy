fe451116ffcb12621600310b6d4ca9b6316494ff
ShaoZhengjiang <shaozhengjiang@gmail.com>
2016-08-15 23:16:16 +0800

    Add atom type info output.

--- scripts/zpe.py
+++ scripts/zpe.py
@@ -10,14 +10,21 @@ if "__main__" == __name__:
     freq_types = outcar.freq_types
 
     # Frequency info.
-    _logger.info("{:<10s}{:<20s}".format("atom", "freq_type"))
-    _logger.info("-"*25)
+    _logger.info("{:<10s}{:<10s}{:<20s}".format("atom", "type", "freq_type"))
+    _logger.info("-"*35)
+
+    # Get atom types.
+    atom_types = []
+    for t, n in zip(poscar.atoms, poscar.atoms_num):
+        atom_types += [t]*n
 
     idx = 0
     tfs = poscar.tf.tolist()
     for atom_idx, tf in enumerate(tfs):
         if tf == ["T", "T", "T"]:
-            _logger.info("{:<10d}{:<5s}{:<5s}{:<5s}".format(atom_idx+1, *freq_types[idx]))
+            msg = "{:<10d}{:<10s}{:<5s}{:<5s}{:<5s}"
+            msg = msg.format(atom_idx+1, atom_types[atom_idx], *freq_types[idx])
+            _logger.info(msg)
             idx += 1
 
     # Zero point energy.
