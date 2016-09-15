e12f29ea6593ee8c05b7b6590e9a4adf950a6f46
pytlab <shaozhengjiang@gmail.com>
2016-09-15 22:09:50 +0800

    Update setup function parameters.

--- setup.py
+++ setup.py
@@ -1,43 +1,65 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 from vaspy import __version__ as version
 
 maintainer = 'Shao-Zheng-Jiang'
 maintainer_email = 'shaozhengjiang@gmail.com'
 author = maintainer
 author_email = maintainer_email
-description = __doc__
+description = "A pure Python library designed to make it easy and quick to manipulate VASP files"
 
 requires = [
-    'numpy',
-    'matplotlib'
+    'numpy>=1.11.1',
+    'matplotlib>=1.5.2',
+    'scipy>=0.18.0',
 ]
 
 license = 'LICENSE'
-with open("README.md") as f:
-    long_description = f.read()
-name = 'python-vaspy'
+
+# Get long description.
+with open("README.rst") as f:
+    lines = f.readlines()
+
+long_description = ""
+for line in lines:
+    if "Installation" in line:
+        break
+    else:
+        long_description += line
+
+name = 'vaspy'
 packages = [
     'vaspy',
-    'scripts',
-    'unittest',
 ]
-data_files = []
 platforms = ['linux']
 url = 'https://github.com/PytLab/VASPy'
 download_url = ''
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Topic :: Text Processing',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+]
 
 setup(author=author,
       author_email=author_email,
       description=description,
       license=license,
       long_description=long_description,
+      setup_requires=requires,
       maintainer=maintainer,
       name=name,
-      packages=packages,
-      data_files=data_files,
+      packages=find_packages(),
+      package_data=package_data,
       platforms=platforms,
       url=url,
       download_url=download_url,
-      version=version)
+      version=version,
+      test_suite="tests",
+      classifiers=classifiers)
+
