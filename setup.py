c31f6e44390003644c2f6896de016ec61b52c3fb
PytLab <shaozhengjiang@gmail.com>
2016-09-18 14:04:06 +0800

    Change long description.

--- setup.py
+++ setup.py
@@ -10,6 +10,55 @@ author = maintainer
 author_email = maintainer_email
 description = "A pure Python library designed to make it easy and quick to manipulate VASP files"
 
+long_description = """
+=====
+VASPy
+=====
+
+.. image:: https://travis-ci.org/PytLab/VASPy.svg?branch=master
+    :target: https://travis-ci.org/PytLab/VASPy
+    :alt: Build Status
+
+.. image:: https://img.shields.io/badge/python-3.5-green.svg
+    :target: https://www.python.org/downloads/release/python-351/
+    :alt: platform
+
+.. image:: https://img.shields.io/badge/python-2.7-green.svg
+    :target: https://www.python.org/downloads/release/python-2710
+    :alt: platform
+
+.. image:: https://img.shields.io/github/stars/PytLab/VASPy.svg
+    :target: https://github.com/PytLab/VASPy/stargazers
+
+.. image:: https://img.shields.io/github/forks/PytLab/VASPy.svg
+    :target: https://github.com/PytLab/VASPy/network
+
+
+Introduction
+------------
+
+VASPy is a pure Python library designed to make it easy and quick to manipulate VASP files.
+
+You can use VASPy to manipulate VASP files in command lins or write your own python scripts to process VASP files and visualize VASP data.
+
+In `/scripts <https://github.com/PytLab/VASPy/tree/master/scripts>`_ , there are some scripts written by me for daily use.
+
+Installation
+------------
+1. Via pip(recommend)::
+
+    pip install vaspy
+
+2. Via easy_install::
+
+    easy_install vaspy
+
+3. From source::
+
+    python setup.py install
+
+"""
+
 requires = [
     'numpy>=1.11.1',
     'matplotlib>=1.5.2',
@@ -19,15 +68,15 @@ requires = [
 license = 'LICENSE'
 
 # Get long description.
-with open("README.rst") as f:
-    lines = f.readlines()
-
-long_description = ""
-for line in lines:
-    if "Installation" in line:
-        break
-    else:
-        long_description += line
+#with open("README.rst") as f:
+#    lines = f.readlines()
+#
+#long_description = ""
+#for line in lines:
+#    if "Installation" in line:
+#        break
+#    else:
+#        long_description += line
 
 name = 'vaspy'
 packages = [
@@ -61,4 +110,3 @@ setup(author=author,
       version=version,
       test_suite="tests",
       classifiers=classifiers)
-
