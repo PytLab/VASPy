f19d4eaec9681192eb761758b1506638b78a5e15
pytlab <shaozhengjiang@gmail.com>
2016-12-28 16:10:32 +0800

    Change the testdata path to relative path.

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,5 +3,6 @@ import os
 
 # Get testdata absolute path.
 abs_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-path = abs_path + "/testdata"
+#path = abs_path + "/testdata"
+path = "./testdata"
 
