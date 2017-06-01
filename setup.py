d8d33c68abb7c51f2389117a4bcd3f67ebb071c0
pytlab <shaozhengjiang@gmail.com>
2017-06-01 19:34:18 +0800

    Update version number.

--- setup.py
+++ setup.py
@@ -23,11 +23,14 @@ VASPy
    :target: https://landscape.io/github/PytLab/VASPy/master
    :alt: Code Health
 
+.. image:: https://codecov.io/gh/PytLab/VASPy/branch/master/graph/badge.svg
+  :target: https://codecov.io/gh/PytLab/VASPy
+
 .. image:: https://img.shields.io/badge/python-3.5, 2.7-green.svg
     :target: https://www.python.org/downloads/release/python-351/
     :alt: platform
 
-.. image:: https://img.shields.io/badge/pypi-v0.8.1-blue.svg
+.. image:: https://img.shields.io/badge/pypi-v0.8.2-blue.svg
     :target: https://pypi.python.org/pypi/vaspy/
     :alt: versions
 
